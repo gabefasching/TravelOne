199af5e64456d459692db05eb9678dce5f2fa5cb
likhithb <lxb326@psu.edu>
2023-04-02 06:48:31 -0400

    Monkey Mode vol 7.

--- frontend/src/HotelPage.js
+++ frontend/src/HotelPage.js
@@ -12,7 +12,26 @@ function HotelPage() {
   const [checkOut, setCheckOut] = useState('');
   const [guests, setGuests] = useState('');
   const [rooms, setRooms] = useState('');
-  const [hotels,setHotels] = useState([]);
+  const [hotels,setHotels] = useState([
+    {
+      'image': '',
+      'savings': '',
+      'discription': '',
+      'bookingUrl': '',
+    },
+    {
+      'image': '',
+      'savings': '',
+      'discription': '',
+      'bookingUrl': '',
+    },
+    {
+      'image': '',
+      'savings': '',
+      'discription': '',
+      'bookingUrl': '',
+    }
+  ]);
   const [leavingFlights,setLeavingFlights] = useState([]);
   const [returningFlights,setReturningFlights] = useState([]);
   
@@ -33,9 +52,9 @@ function HotelPage() {
   };
 //url: 'https://capitaloneshopping.com/api/v1/hotels/search?location='+location+'&checkIn='+checkIn+'&checkOut='+checkOut+'&guests='+guests+'&children=0&rooms=1&searchId=8f90aef2-9d69-4f61-b89f-3810405235a4',
 
-useEffect(() => {
+const useEffect = (event) => {
   const  fetchLocationData = async () => {
-    await axios.post('http://localhost:3000/hotelAPI/hotels', {"location":location,"checkIn":checkIn,"checkOut":checkOut, "guests":guests,"rooms":rooms}).then((response) => {
+    await axios.post('http://localhost:3000/hotelAPI/hotels', {data:{"location":location,"checkIn":checkIn,"checkOut":checkOut, "guests":guests,"rooms":rooms}}).then((response) => {
       console.log(response);
       setHotels(response.data);
 
@@ -44,7 +63,7 @@ useEffect(() => {
   fetchLocationData();
 
   
-}, []);
+};
 
 
   //will update react view everytime useEffect is called
@@ -85,21 +104,21 @@ useEffect(() => {
       <div class="hotels">
 
         <div class="hotelBox"> 
-          <img class="hotelImg" src={hotels[0].img}/> 
+          <img class="hotelImg" src={hotels[0].image}/> 
           <div class = "savings">{hotels[0].savings}</div>
           <div class = "disc"> {hotels[0].discription}</div>
           <a href={hotels[0].bookingUrl} class = "link">Click Here to Book!</a>
         </div>
 
         <div class="hotelBox"> 
-          <img class="hotelImg" src={hotels[1].img}/> 
+          <img class="hotelImg" src={hotels[1].image}/> 
           <div class = "savings">{hotels[1].savings}</div>
           <div class = "disc"> {hotels[1].discription}</div>
           <a href={hotels[1].bookingUrl} class = "link">Click Here to Book!</a>
         </div>
 
         <div class="hotelBox"> 
-          <img class="hotelImg" src={hotels[2].img}/> 
+          <img class="hotelImg" src={hotels[2].image}/> 
           <div class = "savings">{hotels[2].savings}</div>
           <div class = "disc"> {hotels[2].discription}</div>
           <a href={hotels[2].bookingUrl} class = "link">Click Here to Book!</a>
--- hotelAPI.js
+++ hotelAPI.js
@@ -7,19 +7,20 @@ const bParse = require('body-parser')
 const router = express.Router()
 
 router.post("/hotels", async (req, res) => {
+    console.log(req.body.data.location);
     const response = await axios({
       method: "get",
       url:
         "https://capitaloneshopping.com/api/v1/hotels/search?location=" +
-        req.location +
+        req.body.data.location +
         "&checkIn=" +
-        req.checkIn +
+        req.body.data.checkIn +
         "&checkOut=" +
-        req.checkOut +
+        req.body.data.checkOut +
         "&guests=" +
-        req.guests +
+        req.body.data.guests +
         "&children=0&rooms=" +
-        req.rooms +
+        req.body.data.rooms +
         "&searchId=8f90aef2-9d69-4f61-b89f-3810405235a4",
   
       headers: {
@@ -28,7 +29,7 @@ router.post("/hotels", async (req, res) => {
           'wb_session=WB qyHBLlBjgvnF9sR6O66FXMntFoKbdf3XzF7XXsYjtqYtOKoD81XkuNALH1XOrR+BzyLcypIjPW8Oq8Z52b4/nqfVlLPYrf1KHaVGX4AOuW4f0AaVxXIdTHNUAwVug8vgxYV731V36HKThnAjxHxa+qvg+hM2SmjkwAfB7bPJl6vOjvaiyV2qJcDJGQDHGDOz4lAQcMfB7djDlu43foWYtKKQMRiDwdi2/3RQdcre5c7//IK684Trxl9XOh4uTXBYKQtWrIcYPiJYrwtZzN2Xtr68N5ECLTJbQB5R; __cf_bm=BfoMf4XsqyGF2.RIiyRVu2OfOS3IdXyYlYNV0uIlZGA-1680402551-0-AfwcO4Be7IOz65MUNvPwbbaGhKTOgY+c2tutL7r+HRL5ad9KKhx3b+/cD4jrO08hDpv/9xD1xFK4i1pksOuMA5w=; w82S5kL1=ALUTzD-HAQAAAlgLxgYznJgUVG5W8RVFNfxDajQkglLl6j4oGkkfqf4fncb8|1|0|d3adc96e91af3489456c302c6a397e4adfdc2121; ajs_group_id=null; ajs_user_id="f8bedbf1-2ea7-5ace-80a5-7783de7da8a0"; ajs_anonymous_id="dacc6005-b669-4b62-82b1-2cc8301d7449"; _dd_s=rum=0&expire=1680403755161',
       },
     });
-  
+    console.log(response);
     let diff = 0;
     var savingList = [];
     var offersList = [];
@@ -57,26 +58,26 @@ router.post("/hotels", async (req, res) => {
   
     hotels = [
       {
-        image: hotel1.mainImages[0],
-        savings: "You can save $" + parseFloat(list1[1]).toFixed(2),
-        discription: hotel1.areaDescription,
-        bookingUrl: hotel1.offers[list1[2]].bookingUrl,
+        'image': hotel1.mainImages[0],
+        'savings': "You can save $" + parseFloat(list1[1]).toFixed(2),
+        'discription': hotel1.areaDescription,
+        'bookingUrl': hotel1.offers[list1[2]].bookingUrl,
       },
       {
-        image: hotel2.mainImages[0],
-        savings: "You can save $" + parseFloat(list2[1]).toFixed(2),
-        discription: hotel2.areaDescription,
-        bookingUrl: hotel2.offers[list2[2]].bookingUrl,
+        'image': hotel2.mainImages[0],
+        'savings': "You can save $" + parseFloat(list2[1]).toFixed(2),
+        'discription': hotel2.areaDescription,
+        'bookingUrl': hotel2.offers[list2[2]].bookingUrl,
       },
       {
-        image: hotel3.mainImages[0],
-        savings: "You can save $" + parseFloat(list3[1]).toFixed(2),
-        discription: hotel3.areaDescription,
-        bookingUrl: hotel3.offers[list3[2]].bookingUrl,
+        'image': hotel3.mainImages[0],
+        'savings': "You can save $" + parseFloat(list3[1]).toFixed(2),
+        'discription': hotel3.areaDescription,
+        'bookingUrl': hotel3.offers[list3[2]].bookingUrl,
       },
     ];
   
-    res.JSON(hotels);
+    res.json(hotels);
   });
 
   module.exports = router;
