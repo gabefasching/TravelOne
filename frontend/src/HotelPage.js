0b911cd5c3d415fa83c3587656aa6a51d52e3c7d
likhithb <lxb326@psu.edu>
2023-04-02 08:16:56 -0400

    stuff

--- frontend/src/HotelPage.js
+++ frontend/src/HotelPage.js
@@ -12,26 +12,28 @@ function HotelPage() {
   const [checkOut, setCheckOut] = useState('');
   const [guests, setGuests] = useState('');
   const [rooms, setRooms] = useState('');
-  const [hotels,setHotels] = useState([
-    {
-      'image': '',
-      'savings': '',
-      'discription': '',
-      'bookingUrl': '',
-    },
-    {
-      'image': '',
-      'savings': '',
-      'discription': '',
-      'bookingUrl': '',
-    },
-    {
-      'image': '',
-      'savings': '',
-      'discription': '',
-      'bookingUrl': '',
-    }
-  ]);
+  const [hotels,setHotels] = useState(
+    [
+      {
+          "image": "https://images.trvl-media.com/lodging/40000000/39190000/39186300/39186206/533f153a.jpg?impolicy=fcrop&w=1000&h=666&quality=medium",
+          "savings": "You can save $93.61",
+          "discription": "Cour des Vosges is located in Paris City Center, a neighborhood in Paris, and is in a shopping district and near a metro station. Louvre Museum and Palais Garnier are cultural highlights, and some of the area's notable landmarks include Notre-Dame and Paris Catacombs. Check out an event or a game at Parc des Princes, and consider making time for Luxembourg Gardens, a top attraction not to be missed. ",
+          "bookingUrl": "https://www.travelocity.com/go/hotel/info/39186206/2023-05-01/2023-05-02?tpid=80001&eapid=84080&NumAdult-Room1=2&rateplanid=274233090_24&currency=USD&mrp=0&langid=1033&NumRooms=1"
+      },
+      {
+          "image": "https://images.trvl-media.com/lodging/1000000/90000/83100/83021/426bfe74.jpg?impolicy=fcrop&w=1000&h=666&quality=medium",
+          "savings": "You can save $87.39",
+          "discription": "In the entertainment district, Esprit Saint-Germain is in Paris City Center, a walkable area in Paris with good shopping. Louvre Museum and Palais Garnier are cultural highlights, and some of the area's notable landmarks include Notre-Dame and Arc de Triomphe. Check out an event or a game at Parc des Princes, and consider making time for Luxembourg Gardens, a top attraction not to be missed. Guests love the hotel's location for the sightseeing. ",
+          "bookingUrl": "https://www.travelocity.com/go/hotel/info/83021/2023-05-01/2023-05-02?tpid=80001&eapid=84080&NumAdult-Room1=2&rateplanid=381229787_24&currency=USD&mrp=0&langid=1033&NumRooms=1"
+      },
+      {
+          "image": "https://images.trvl-media.com/lodging/6000000/5580000/5576300/5576207/d0bcdca6.jpg?impolicy=fcrop&w=1000&h=666&quality=medium",
+          "savings": "You can save $72.63",
+          "discription": "Located in Paris City Center neighborhood, Grand Hotel du Palais Royal is connected to a rail/subway station. Louvre Museum and Palais Garnier are cultural highlights, and some of the area's notable landmarks include Palais Royal and Notre-Dame. Traveling with kids? Make time for Luxembourg Gardens, or check out an event or a game at Stade de France. Guests love the hotel's location for the sightseeing. ",
+          "bookingUrl": "https://www.travelocity.com/go/hotel/info/5576207/2023-05-01/2023-05-02?tpid=80001&eapid=84080&NumAdult-Room1=2&rateplanid=382334311_24&currency=USD&mrp=0&langid=1033&NumRooms=1"
+      }
+  ]
+  );
   const [leavingFlights,setLeavingFlights] = useState([]);
   const [returningFlights,setReturningFlights] = useState([]);
   
@@ -124,10 +126,6 @@ const useEffect = (event) => {
           <a href={hotels[2].bookingUrl} class = "link">Click Here to Book!</a>
         </div>
       </div>
-
-        
-        
-
       <div class="leaving"></div>
       <div class="returning"></div>
     </body>
