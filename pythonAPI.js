5f3cb1805627f171b01a3ffd4dc225a2e02a4f99
likhithb <lxb326@psu.edu>
2023-04-02 08:16:38 -0400

    No monkeys

--- pythonAPI.js
+++ pythonAPI.js
@@ -3,8 +3,19 @@ const express = require('express')
 const axios = require('axios')
 const cors = require('cors')
 const bParse = require('body-parser')
-
+const { PythonShell } = require('python-shell');
 const router = express.Router()
 
+router.post("/airlines", async (req, res) => {
 
+});
+
+router.post("/capInfo", async (req, res) => {
+    const Id = req.body.Id;
+    const endpoint = 'http://api.nessieisreal.com/customers?key=6aad83d07cf9e01164f8cfb5a02eb362';
+    const api_key = '6aad83d07cf9e01164f8cfb5a02eb362';
+    const getEndpoint = 'http://api.nessieisreal.com/customers/'+Id+'?key='+api_key;
+    const response = await axios.get(getEndpoint);
+    res.json(response.text)
+});
 module.exports = router;
